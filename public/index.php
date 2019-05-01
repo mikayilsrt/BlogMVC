148d25a9f7ac6ba948f0dae5f44b7a9f1d516e75
Mikayil S <sert.mikayil@gmail.com>
2019-05-01 15:27:32 +0200

    feat(Routes): add the router of slim framework

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\config\database;
+use Slim\App as Router;
 
 require "../vendor/autoload.php";
 
@@ -11,3 +12,11 @@ define("DB_PASSWORD", "");
 
 $database = new database(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
 $database->getPDO();
+
+$app = new Router();
+
+$app->get('/', function ($request, $response, $args) {
+    echo "Hello World";
+});
+
+$app->run();
