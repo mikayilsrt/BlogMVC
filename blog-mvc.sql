e51141ea8d2260a80aaab8ec4c31702e19d3db04
Mikayil S <sert.mikayil@gmail.com>
2019-05-08 17:45:58 +0200

    feat(SQL): change sql file with new tabs

--- blog-mvc.sql
+++ blog-mvc.sql
@@ -16,6 +16,27 @@
 CREATE DATABASE IF NOT EXISTS `blog-mvc` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `blog-mvc`;
 
+-- Export de la structure de la table blog-mvc. articles
+CREATE TABLE IF NOT EXISTS `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT '0',
+  `content` text,
+  `thumbnail` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Export de données de la table blog-mvc.articles : ~0 rows (environ)
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+INSERT INTO `articles` (`id`, `title`, `content`, `thumbnail`, `created_at`, `updated_at`, `user_id`) VALUES
+	(1, 'Le Lorem Ipsum est simplement du faux texte', 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.', 'images-1.png', '2019-05-08 16:56:07', '2019-05-08 16:56:08', 33),
+	(2, 'Le Lorem Ipsum est simplement du faux texte', 'Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.', 'images-1.png', '2019-05-08 16:56:07', '2019-05-08 16:56:08', 33);
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+
 -- Export de la structure de la table blog-mvc. users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,12 +46,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
 
--- Export de données de la table blog-mvc.users : ~0 rows (environ)
+-- Export de données de la table blog-mvc.users : ~1 rows (environ)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-	(22, 'mikayilsrt', 'sert.mikayil@gmail.com', 'dfc13a839cee137559aa880a32649035', '2019-05-02 22:42:16', '2019-05-02 22:42:16');
+	(33, 'root', 'root@gmail.com', 'dfc13a839cee137559aa880a32649035', '2019-05-06 19:38:17', '2019-05-06 19:38:17');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
